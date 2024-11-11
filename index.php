e4c73e8ae969e417f793ef70f1f0a5092f027351
Hong Anh Quan <Anh.Quan.220703@gmail.com>
2024-11-11 20:32:24 +0700

    2024/11/11

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
 session_start();
 // Process URL from browser
-require_once "./Function/App.php";
+require_once "Function/App.php";
 
 // How controllers call Views & Models
-require_once "./Function/controller.php";
+require_once "Function/controller.php";
 
 // Connect Database
-require_once "./Function/DB.php";
+require_once "Function/DB.php";
 $myApp = new App();
